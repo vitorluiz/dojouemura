76cce1c5dc24b344fc45fab5b0a18908e599cbe5
Vitor Luiz Machado <vitorluizmachado@gmail.com>
2025-08-17 16:22:42 -0400

    fix: adicionar imports dos modelos TipoMatricula, Modalidade e StatusMatricula - resolver erro 'name not defined' na view matricula_projeto_social

--- usuarios/views.py
+++ usuarios/views.py
@@ -9,7 +9,7 @@ from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.encoding import force_bytes, force_str
 from django.contrib.auth.tokens import default_token_generator
 from django.http import HttpResponse, JsonResponse
-from .models import Usuario, Dependente
+from .models import Usuario, Dependente, TipoMatricula, Modalidade, StatusMatricula
 from .forms import UsuarioRegistroForm, UsuarioLoginForm, DependenteForm
 import requests
 from datetime import date, datetime
